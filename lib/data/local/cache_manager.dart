c214d0f8e6543a8b866e1668ac6120f4a4f13538
Z-oybek <newprogrammer2023@gmail.com>
2024-04-25 14:06:18 +0500

    kk

--- lib/data/local/cache_manager.dart
+++ lib/data/local/cache_manager.dart
@@ -4,6 +4,7 @@ class CacheManager {
   Future<bool> getIntroState() async {
     final shared = await SharedPreferences.getInstance();
     return shared.getBool('intro') ?? false;
+
   }
   Future<void> saveIntroState(bool state) async {
     final shared = await SharedPreferences.getInstance();
